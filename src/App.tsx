7679cbce36310cadd36159c3a86f35ad189f4279
DmitriyKovchugo <dkovchugo@mail.ru>
2023-11-21 16:43:57 +0300

    start project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, rem dolorum magni earum rerum neque expedita iste alias, quis distinctio odio ad dolores veritatis obcaecati iure illo voluptas nisi, soluta reprehenderit accusantium. Magni vitae nisi recusandae beatae ut, iure quo quos. Odio maxime, nesciunt laboriosam aperiam iste reiciendis. Maiores pariatur alias tempore? Iure exercitationem vel, expedita impedit blanditiis nesciunt, excepturi incidunt rerum neque debitis architecto deleniti explicabo perspiciatis minima fugit! Ad iure iste repellendus animi in ratione nam cum porro ipsum, eaque quidem quisquam voluptas hic culpa praesentium! Dolor nemo voluptatem quis aut a rem accusamus hic itaque fugiat explicabo nobis, officia odio modi cum quaerat recusandae distinctio obcaecati inventore culpa commodi aliquam ullam voluptatum! Qui a sit ducimus tenetur cumque soluta ex? Quod quia nesciunt voluptatem rem, molestias quam. Repellendus nihil quia vel, atque, totam minima at ducimus delectus exercitationem, aliquam beatae ipsa illum debitis animi quam aspernatur in officiis amet error pariatur sit veniam provident. Et laudantium sit eius cupiditate placeat natus! Nobis, doloribus voluptatum tenetur nulla similique, recusandae voluptas repellendus libero temporibus dolore minima ipsum nisi aut illo ex? Modi veritatis consectetur, nisi hic, possimus rem nesciunt reiciendis eius explicabo asperiores, ipsa tenetur in animi. Aspernatur, dicta?
+    </>
+  )
+}
+
+export default App
