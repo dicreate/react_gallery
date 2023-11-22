b0395574ec6b5e36afd98bf0eed1161b86d71f7f
DmitriyKovchugo <dkovchugo@mail.ru>
2023-11-22 12:39:44 +0300

    add styles for PreviewGallery

--- src/Components/Gallery/PreviewGallery/index.tsx
+++ src/Components/Gallery/PreviewGallery/index.tsx
@@ -1,11 +1,33 @@
 import { Photo, CommonClassProps } from "../types";
-
+import cl from "classnames"
+import style from './index.module.scss'
 interface PreviewGalleryProps extends CommonClassProps {
    activePhotoIndex: number;
    photos: Photo[];
    
 }
 
-export const PreviewGallery: React.FC<PreviewGalleryProps> = ({ activePhotoIndex, photos}) => (
-   <div>Preview Gallery</div>
+export const PreviewGallery: React.FC<PreviewGalleryProps> = ({ activePhotoIndex, photos, className}) => (
+   <div className={cl(style.previewGallery, className)}>
+      <ul
+         className={style.previewGalleryTrack}
+      >
+         {
+            photos.map((photo) => (
+               <li
+                  key={photo.id}
+                  className={style.previewGalleryPreview}
+               >
+                  <img 
+                     src={photo.preview} 
+                     alt={photo.description}
+                     className={style.previewGalleryImage} />
+               </li>
+            ))
+         }
+      </ul>
+      <div className={style.previewGalleryCover}>
+         {activePhotoIndex + 1} / {photos.length}
+      </div>
+   </div>
 );
