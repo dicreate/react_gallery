c317d57a92b943ab0a3905962b9c874b34c7a059
DmitriyKovchugo <dkovchugo@mail.ru>
2023-12-09 13:02:50 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,16 @@ function App() {
 
   return (
     <>
-      <Gallery 
+      TEST
+      <Gallery
         photos={[
-        {id: 1, src: "/react_gallery/photos/1.jpg", preview: "/react_gallery/photos/preview/1.jpg", description: "photo 1"},
-        {id: 2, src: "/react_gallery/photos/2.jpg", preview: "/react_gallery/photos/preview/2.jpg", description: "photo 2"},
-        {id: 3, src: "/react_gallery/photos/3.jpg", preview: "/react_gallery/photos/preview/3.jpg", description: "photo 3"},
-        {id: 4, src: "/react_gallery/photos/4.jpg", preview: "/react_gallery/photos/preview/4.jpg", description: "photo 4"},
-        {id: 5, src: "/react_gallery/photos/5.jpg", preview: "/react_gallery/photos/preview/5.jpg", description: "photo 5"},
-        {id: 6, src: "/react_gallery/photos/6.jpg", preview: "/react_gallery/photos/preview/6.jpg", description: "photo 6"},
-        {id: 7, src: "/react_gallery/photos/7.jpg", preview: "/react_gallery/photos/preview/7.jpg", description: "photo 7"},
+          { id: 1, src: "/react_gallery/photos/1.jpg", preview: "/react_gallery/photos/preview/1.jpg", description: "photo 1" },
+          { id: 2, src: "/react_gallery/photos/2.jpg", preview: "/react_gallery/photos/preview/2.jpg", description: "photo 2" },
+          { id: 3, src: "/react_gallery/photos/3.jpg", preview: "/react_gallery/photos/preview/3.jpg", description: "photo 3" },
+          { id: 4, src: "/react_gallery/photos/4.jpg", preview: "/react_gallery/photos/preview/4.jpg", description: "photo 4" },
+          { id: 5, src: "/react_gallery/photos/5.jpg", preview: "/react_gallery/photos/preview/5.jpg", description: "photo 5" },
+          { id: 6, src: "/react_gallery/photos/6.jpg", preview: "/react_gallery/photos/preview/6.jpg", description: "photo 6" },
+          { id: 7, src: "/react_gallery/photos/7.jpg", preview: "/react_gallery/photos/preview/7.jpg", description: "photo 7" },
         ]} />
     </>
   )
